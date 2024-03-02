36e4c24ea5136c03cb8792c9f8943e9d4244d967
lielysantana12 <c.santana59@unapec.edu.do>
2024-03-02 16:01:25 -0400

    new commit segundo parcial

---
+++ bibliotecaProject/Models/seedEmployees.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using bibliotecaProject.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace bibliotecaProject.Models
+{
+    public static class seedEmployees
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new PagesEmpleadosContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<PagesEmpleadosContext>>()))
+            {
+                //if (context == null || context.empleados == null)
+                //{
+                //    throw new ArgumentNullException("Null PagesEmpleadosContext");
+                //}
+
+                //// Look for any movies.
+                //if (context.e.Any())
+                //{
+                //    return;   // DB has been seeded
+                //}
+
+                //context.empleados.AddRange(
+                //    new empleados
+                //    {
+                //        Id = 1,
+                //        Nombre = "Juan Perez",
+                //        Cedula = "4022287777",
+                //        TandaLabor = "PM",
+                //        PorcientoComision = "15"
+
+                //    }
+                //);
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- bibliotecaProject/Program.cs
+++ bibliotecaProject/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using bibliotecaProject.Data;
+using bibliotecaProject.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -25,6 +26,13 @@ builder.Services.AddDbContext<PagesAutoresContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("PagesAutoresContext") ?? throw new InvalidOperationException("Connection string 'PagesAutoresContext' not found.")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    seedEmployees.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
